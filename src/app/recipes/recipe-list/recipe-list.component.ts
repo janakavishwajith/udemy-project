6fb7c8f2e830a1a624b5dcb0875a85bab798e4e7
janakavishwjith <janakan@codegen.net>
2017-07-12 19:04:57 +0530

    commit changes at office

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Recipe } from '../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe( "A Test Recipe", "this is simply a test", "http://myrecipebox.co.nz/wp-content/uploads/2016/07/RecipeCarousel-Coconut-Curry-Soup.jpg")
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
